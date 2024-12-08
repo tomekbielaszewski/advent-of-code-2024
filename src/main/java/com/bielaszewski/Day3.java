2eb0b959adeb4383dc4fb15194a7734fca2155f4
tomekbielaszewski <tomekbielaszewski@gmail.com>
2024-12-08 18:07:32 +0100

    day 3 part 2

--- src/main/java/com/bielaszewski/Day3.java
+++ src/main/java/com/bielaszewski/Day3.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 
 public class Day3 implements Advent.OfCode {
 
-    private String start = "mul(";
-    private char mulEnd = ')';
-    private char mulSep = ',';
+    private final String mulStart = "mul(";
+    private final String enabler = "do()";
+    private final String disabler = "don't()";
+    private final char mulEnd = ')';
+    private final char mulSep = ',';
 
     @Override
     public int solve1(String input) {
@@ -56,17 +58,13 @@ public class Day3 implements Advent.OfCode {
 
     private int nextMul(int index, char[] input) {
         for (int i = index; i < input.length; i++) {
-            if (isMul(i, input)) return i + start.length();
+            if (isMul(i, input)) return i + mulStart.length();
         }
         return -1;
     }
 
-
     private boolean isMul(int index, char[] input) {
-        for (int i = 0; i < start.length() && index + i < input.length; i++) {
-            if (input[index + i] != start.charAt(i)) return false;
-        }
-        return true;
+        return isWord(mulStart, index, input);
     }
 
     private class NumberDetection {
@@ -77,6 +75,55 @@ public class Day3 implements Advent.OfCode {
 
     @Override
     public int solve2(String input) {
-        return 0;
+        List<Integer> numbers = new ArrayList<>();
+        char[] input_ = input.toCharArray();
+        boolean isEnabled = true;
+
+        for (int i = 0; i < input_.length; i++) {
+            if (isEnabled && isMul(i, input_)) {
+                i = nextMul(i, input_);
+                if (i == -1) break;
+                if (i >= input_.length) break;
+
+                NumberDetection nd1 = nextNum(i, input_);
+                if (!nd1.detected) continue;
+                if (nd1.endIndex >= input_.length) break;
+                if (input_[nd1.endIndex] != mulSep) continue;
+                i = nd1.endIndex + 1;
+
+                NumberDetection nd2 = nextNum(i, input_);
+                if (!nd2.detected) continue;
+                if (nd2.endIndex >= input_.length) break;
+                if (input_[nd2.endIndex] != mulEnd) continue;
+                i = nd2.endIndex;
+
+                numbers.add(nd1.num * nd2.num);
+            }
+            if (isEnabled && isDont(i, input_)) {
+                isEnabled = false;
+                i += disabler.length() - 1;
+            }
+            if (!isEnabled && isDo(i, input_)) {
+                isEnabled = true;
+                i += enabler.length() - 1;
+            }
+        }
+
+        return numbers.stream().reduce(0, Integer::sum);
+    }
+
+    private boolean isDont(int index, char[] input) {
+        return isWord(disabler, index, input);
+    }
+
+    private boolean isDo(int index, char[] input) {
+        return isWord(enabler, index, input);
+    }
+
+    private boolean isWord(String word, int index, char[] input) {
+        for (int i = 0; i < word.length() && index + i < input.length; i++) {
+            if (input[index + i] != word.charAt(i)) return false;
+        }
+        return true;
     }
 }
--- src/test/java/com/bielaszewski/Tests.java
+++ src/test/java/com/bielaszewski/Tests.java
@@ -14,7 +14,13 @@ public class Tests {
     private int[] expectedTestResults = new int[]{
             11, 31, //day 1
             2, 4,   //day 2
-            161, 0  //day 3
+            161, 48  //day 3
+    };
+
+    private String[] inputs = new String[]{
+            "day1test1", "day1test1", "day1input", //day 1
+            "day2test1", "day2test1", "day2input", //day 2
+            "day3test1", "day3test2", "day3input", //day 3
     };
 
     @Test
@@ -22,18 +28,19 @@ public class Tests {
         for (int i = 0; i < days.length; i++) {
             Advent.OfCode day = days[i];
 
-            String testInput = Advent.read(String.format("day%dtest", i+1));
-            int testResult1 = day.solve1(testInput);
+            String testInput1 = Advent.read(inputs[i * 3]);
+            int testResult1 = day.solve1(testInput1);
             int expectedTestResult1 = expectedTestResults[i * 2];
-            assertEquals(expectedTestResult1, testResult1, String.format("Result of 1st test on Day %d is incorrect. Expected: %d, Actual %d", i+1,expectedTestResult1, testResult1));
+            assertEquals(expectedTestResult1, testResult1, String.format("Result of 1st test on Day %d is incorrect. Expected: %d, Actual %d", i + 1, expectedTestResult1, testResult1));
 
-            String input = Advent.read(String.format("day%dinput", i+1));
+            String input = Advent.read(inputs[i * 3 + 2]);
             int result1 = day.solve1(input);
             System.out.printf("First star of Day %d is: %d\n", i + 1, result1);
 
-            int testResult2 = day.solve2(testInput);
+            String testInput2 = Advent.read(inputs[i * 3 + 1]);
+            int testResult2 = day.solve2(testInput2);
             int expectedTestResult2 = expectedTestResults[i * 2 + 1];
-            assertEquals(expectedTestResult2, testResult2, String.format("Result of 2nd test on Day %d is incorrect. Expected: %d, Actual %d", i+1,expectedTestResult2, testResult2));
+            assertEquals(expectedTestResult2, testResult2, String.format("Result of 2nd test on Day %d is incorrect. Expected: %d, Actual %d", i + 1, expectedTestResult2, testResult2));
 
             int result2 = day.solve2(input);
             System.out.printf("Second star of Day %d is: %d\n", i + 1, result2);
